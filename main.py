f0efb95e70c5a440e09a79a0af04f3c8dee2b5bd
Zander1264 <87423331+Zander1264@users.noreply.github.com>
2025-07-16 17:17:56 +0400

    Added show tasks and async framework for bot usage

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-import telebot
 import os
 import sqlite3
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from contextlib import contextmanager
 
+# чтение токена бота из файла и его объявление
 with open("../tg_bot_token.txt", "r") as file:
-    token = file.readline()
-bot = telebot.TeleBot(token)
+    token = file.readline().strip()
+bot = Bot(token=token)
+dp = Dispatcher(bot)
 
+
+# инициализация бд
 def init_db():
     conn = sqlite3.connect('study.db')
     cursor = conn.cursor()
@@ -13,75 +21,104 @@ def init_db():
                         id INTEGER PRIMARY KEY AUTOINCREMENT, 
                         chat_id TEXT NOT NULL,
                         task TEXT NOT NULL)''')
-    conn.commit()  
+    conn.commit()
     conn.close()
+
 
 HELP = """
 /help - спиоок доступных комманд
-/add-task - Добавить задачу для выполнения"""
+/addTask - Добавить задачу для выполнения
+/showTasks - Показать список запланированных задач"""
+
 
 # Состояния диалога
-class TaskStates:
-    WAITING_FOR_TASK = 1
-
-# Хранилище состояний (можно использовать глобальную переменную для простоты)
-user_states = {}
+class TaskStates(StatesGroup):
+    WAITING_FOR_TASK = State()
 
 
+def get_db_connection():
+    return sqlite3.connect('study.db', check_same_thread=False)
 
-# #фильтр обработки сообщений(декоратор)
-# @bot.message_handler(content_types=["text"])
-# def echo(message):
-#     bot.send_message(message.chat.id, message.text)
+@contextmanager
+def db_session():
+    conn = get_db_connection()
+    try:
+        yield conn
+    finally:
+        conn.close()
 
-@bot.message_handler(commands=["help"])
-def help(message):
-    bot.send_message(message.chat.id, HELP)
+# Кнопка отменить
+cancel_markup = ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton("Отменить"))
 
-@bot.message_handler(commands=["add-task"])
-def add_task(message):
-    # Сохраняем состояние ожидания ввода задачи
-    user_states[message.from_user.id] = TaskStates.WAITING_FOR_TASK
-    
-    # Создаем клавиатуру с кнопкой отмены
-    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add(telebot.types.KeyboardButton("Отменить"))
-    
-    bot.send_message(
-        message.chat.id,
-        "Напишите текст задачи",
-        reply_markup=markup
-    )
+
+# Команда показания задач
+@dp.message_handler(commands=["showTasks"])
+async def show_tasks(message: types.Message):
+    chat_id = str(message.chat.id)
+    try:
+        # Подключение к БД
+        conn = sqlite3.connect('study.db', check_same_thread=False)
+        cursor = conn.cursor()
+        cursor.execute("PRAGMA journal_mode=WAL;")
+        # Получаем задачи пользователя
+        cursor.execute("SELECT task FROM users WHERE chat_id = ?", (chat_id,))
+        tasks = cursor.fetchall()
+
+        if not tasks:
+            await message.reply("У вас нет запланированных задач.")
+        else:
+            response = "Вот все ваши запланированные задачи: \n\n"
+            for idx, task in enumerate(tasks, start=1):
+                response += f"{idx}. {task[0]}\n"
+            await message.reply(response)
+
+    except Exception as e:
+        await message.reply(f"Ошибка при зарузке задач: {str(e)}")
+    finally:
+        conn.close()
+
+
+# команда /help
+@dp.message_handler(commands=["help"])
+async def help(message: types.Message):
+    await message.reply(HELP)
+
+
+# Команда добавить задачу
+@dp.message_handler(commands=["addTask"])
+async def add_task(message: types.Message):
+    await message.reply("Напиши текст задачи, которую хочешь сохранить", reply_markup=cancel_markup)
+    await TaskStates.WAITING_FOR_TASK.set()
+
 
 # Обработчик ввода задачи
-@bot.message_handler(func=lambda m: user_states.get(m.from_user.id) == TaskStates.WAITING_FOR_TASK)
-def save_task(message):
+@dp.message_handler(state=TaskStates.WAITING_FOR_TASK)
+async def save_task(message: types.Message, state: FSMContext):
     if message.text == "Отменить":
-        bot.reply_to(message, "Добавление задачи отменено")
-        del user_states[message.from_user.id]
+        await message.reply("Добавление задачи отменено", reply_markup=types.ReplyKeyboardRemove())
+        await state.finish()
         return
-        
+
     try:
-        conn = sqlite3.connect('study.db')
+        conn = sqlite3.connect('study.db', check_same_thread=False)
         cursor = conn.cursor()
+        cursor.execute("PRAGMA journal_mode=WAL;")
         cursor.execute('''
             INSERT INTO users (chat_id, task)
             VALUES (?, ?)
         ''', (str(message.chat.id), message.text))
         conn.commit()
-        
-        bot.reply_to(message, f"Задача сохранена! ID чата: {message.chat.id}")
-        del user_states[message.from_user.id]
+        await message.reply(f"Задача сохранена!", reply_markup=types.ReplyKeyboardRemove())
     except Exception as e:
-        bot.reply_to(message, f"Ошибка сохранения: {str(e)}")
+        await message.reply(f"Ошибка сохранения: {str(e)}", reply_markup=types.ReplyKeyboardRemove())
     finally:
         conn.close()
-
+        await state.finish()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     init_db()
-    #постоянно обращается к серверам телеграм
-    bot.polling(none_stop=True)
-
+    from aiogram.contrib.fsm_storage.memory import MemoryStorage
+    dp.storage = MemoryStorage()
+    executor.start_polling(dp, skip_updates=True)
